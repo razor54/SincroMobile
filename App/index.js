48f9cb298d8e217c9613ea02b30d2f0800c320c8
razor54 <andregaudencio5@gmail.com>
2018-05-05 14:21:19 +0100

    added eslint

--- App/index.js
+++ App/index.js
@@ -1,6 +1,5 @@
-import {AppRegistry} from 'react-native';
+import { AppRegistry, YellowBox } from 'react-native';
 import App from './src/app/App';
-import {YellowBox} from 'react-native';
 
 
 YellowBox.ignoreWarnings(['Warning: isMounted(...) is deprecated', 'Module RCTImageLoader']);
